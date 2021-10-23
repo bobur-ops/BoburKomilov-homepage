07b506a3fe4e1fd62c4c7828b6e316e2f1ab53fa
bobur-ops <bobur0172@gmail.com>
2021-10-23 23:45:44 +0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,147 @@
+import NextLink from 'next/link'
+import {
+  Button,
+  Container,
+  Box,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Link,
+  Icon,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import { IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+
+const Page = () => {
+  return (
+    <Layout>
+      <Container>
+        <Box
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          align="center"
+        >
+          Hello, I&apos;m a front-end developer based in Uzbekistan!
+        </Box>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Bobur Komilov
+            </Heading>
+            <p>Digital Craftzman ( Developer / Designer )</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              border-color="whiteAplha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/bobur.jpg"
+              alt="Profile Image"
+            />
+          </Box>
+        </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Bobur is a freelance and a full-stack developer based in Uzbekistan
+            with a passion for building digital ae MRM Mt aOR launching
+            products, from planning and designing all the way to solving
+            real-life problems with code. When not online, he loves hanging out
+            with his camera. Currently he is living off his own product called{' '}
+            <NextLink href="/works/inkdrop">
+              <Link>Inkdrop</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2005</BioYear>
+            Born in Ferghana, Uzbekistan.
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Started learning web-developing by self-study
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Began working as a freelancer
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Art, Music, Football, Motion Design, Machine Learning
+          </Paragraph>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            On the web
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/bobur-ops" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoGithub} />}
+                >
+                  @bobur-ops
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                href="https://www.instagram.com/_bobur_x16_"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoInstagram} />}
+                >
+                  @bobur-ops
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Page
