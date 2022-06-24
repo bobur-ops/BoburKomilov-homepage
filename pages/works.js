a7c1784fc1dee736bdd6a8f51c7d221c77435081
Bobur <bobur0172@gmail.com>
2022-06-24 20:23:27 +0500

    deploy

--- pages/works.js
+++ pages/works.js
@@ -2,8 +2,6 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import Layout from '../components/layouts/article'
 import { WorkGridItem } from '../components/grid-item'
-
-import thumbCryptoverse from '../public/images/work/crypto_1.png'
 import thumbCms_blog from '../public/images/work/CMS_1.png'
 import thumbSearch from '../public/images/work/search_1.png'
 import thumbSlider from '../public/images/work/slider.png'
@@ -21,16 +19,6 @@ const Works = () => {
         </Heading>
 
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
-          <Section>
-            <WorkGridItem
-              id="cryptoverse"
-              title="Cryptoverse"
-              thumbnail={thumbCryptoverse}
-            >
-              An application to monitor cryptocurrencies. Also can find latest
-              CryptoNews
-            </WorkGridItem>
-          </Section>
           <Section>
             <WorkGridItem
               id="cms-blog"
