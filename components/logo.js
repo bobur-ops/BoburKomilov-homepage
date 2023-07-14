e851866e518ceec8f4179ed7567251a31bfd6e9d
Muxammadbobur <bobur0172@gmail.com>
2023-07-14 21:44:07 +0500

    fix logo name

--- components/logo.js
+++ components/logo.js
@@ -31,7 +31,7 @@ const Logo = () => {
             fontWeight="bold"
             ml={3}
           >
-            Bobur Komilov
+            Muxammadbobur Komiljonov
           </Text>
         </LogoBox>
       </a>
