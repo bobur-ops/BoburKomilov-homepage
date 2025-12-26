85e329ed0e76340ae7d7ce6ce0b5422b12f3fba5
Muxammadbobur <boburkomilovv@gmail.com>
2025-12-26 22:28:12 +0500

    add trailing slash to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.bobur.me",
+  siteUrl: "https://www.bobur.me/",
   generateRobotsTxt: true,
   sitemapSize: 50000,
   generateIndexSitemap: false,
