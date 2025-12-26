3e53ba62c5e4170b5a9c9eb17f199fac2c94f369
Muxammadbobur <boburkomilovv@gmail.com>
2025-12-26 22:20:39 +0500

    priority for homepage

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,13 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 50000,
   generateIndexSitemap: false,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: path === "/" ? 1.0 : 0.7,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
