1ebde3655c648d5d5662a4a8f66114fea6369d4f
bobur-ops <boburkomilovv@gmail.com>
2026-01-18 16:32:01 +0500

    create astro

--- src/utils/getPostContent.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-
-export async function getPostContent(relativePath: string) {
-  const filePath = path.join(process.cwd(), "src", "app", relativePath);
-  const raw = fs.readFileSync(filePath, "utf-8");
-  return matter(raw);
-}
